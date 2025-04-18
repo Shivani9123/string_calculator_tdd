32a2a7ee897affce18b0416a85345877d4ab0c54
Shivani9123 <Shivaniofficial9123@gmail.com>
2025-04-18 09:28:48 +0000

    add implementation for summing two comma-separated numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,7 +1,7 @@
 require './string_calculator'
 
 RSpec.describe StringCalculator do
-  
+
   describe '.add' do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
@@ -10,5 +10,10 @@ RSpec.describe StringCalculator do
     it 'returns the number itself when a single number is provided' do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it 'returns the sum of two comma-separated numbers' do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
+    
   end
 end
