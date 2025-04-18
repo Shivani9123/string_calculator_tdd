bcae2546063d6d6365fce6612a49cb334e2edd32
Shivani9123 <Shivaniofficial9123@gmail.com>
2025-04-18 10:17:41 +0000

    add implementation for custom delimiters in format //;\n1;2

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,14 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      custom_delimiter = parts[0][2]
+      delimiter = Regexp.union(custom_delimiter)
+      numbers = parts[1]
+    end
+
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
